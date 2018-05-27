ce24f60a8f739903bf3ab05f80dba8a66a0b85e9
Unknown <steve.yeager@littlebytegames.com>
2018-05-27 11:47:32 -0700

    Add image rules

---
+++ ClimbWeb/Climb/General/ImageRules.cs
@@ -0,0 +1,20 @@
+﻿namespace Climb
+{
+    public class ImageRules
+    {
+        public int MaxSize { get; }
+        public int Width { get; }
+        public int Height { get; }
+        public string Folder { get; }
+        public string MissingUrl { get; }
+
+        public ImageRules(int maxSize, int width, int height, string folder, string missingUrl)
+        {
+            MaxSize = maxSize;
+            Width = width;
+            Height = height;
+            Folder = folder;
+            MissingUrl = missingUrl;
+        }
+    }
+}
