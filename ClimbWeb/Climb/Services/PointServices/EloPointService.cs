9fbf69d8a785372da32c61575a6ca8ee071af457
Unknown <steve.yeager@littlebytegames.com>
2018-05-28 21:58:13 -0700

    Add PointService

---
+++ ClimbWeb/Climb/Services/PointServices/EloPointService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Climb.Services
+{
+    public class EloPointService : IPointService
+    {
+        public (int p1Points, int p2Points) CalculatePointDeltas(int p1Points, int p2Points, bool p1Won, int kFactor = 32)
+        {
+            var transformedP1 = GetTransformedScore(p1Points);
+            var transformedP2 = GetTransformedScore(p2Points);
+
+            var expectedP1 = transformedP1 / (transformedP1 + transformedP2);
+            var expectedP2 = transformedP2 / (transformedP1 + transformedP2);
+
+            var scoreP1 = p1Won ? 1 : 0;
+            var scoreP2 = !p1Won ? 1 : 0;
+
+            var eloDeltaP1 = kFactor * (scoreP1 - expectedP1);
+            var eloDeltaP2 = kFactor * (scoreP2 - expectedP2);
+
+            return ((int)Math.Round(eloDeltaP1), (int)Math.Round(eloDeltaP2));
+
+            double GetTransformedScore(int winnerScore)
+            {
+                return Math.Pow(10, (double)winnerScore / 400);
+            }
+        }
+    }
+}
---
+++ ClimbWeb/Climb/Services/PointServices/IPointService.cs
@@ -0,0 +1,7 @@
+﻿namespace Climb.Services
+{
+    public interface IPointService
+    {
+        (int p1Points, int p2Points) CalculatePointDeltas(int p1Points, int p2Points, bool p1Won, int kFactor = 32);
+    }
+}
