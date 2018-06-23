a35d18fa825bf1b9bc47ce551ff59ab8794026a1
Unknown <steve.yeager@littlebytegames.com>
2018-06-23 09:18:21 -0700

    Show challenge button and requests in users/home page

---
+++ ClimbWeb/Climb/ClientApp/scripts/Users-Home.ts
@@ -0,0 +1,17 @@
+﻿function openChallengeModal() {
+    toggleChallengeModal(true);
+}
+
+function closeChallengeModal() {
+    toggleChallengeModal(false);
+}
+
+function toggleChallengeModal(open: Boolean)
+{
+    const challengeModal = document.getElementById("challenge-modal");
+    if (challengeModal == null) {
+        return;
+    }
+
+    challengeModal.hidden = !open;
+}
--- ClimbWeb/Climb/ViewModels/Users/HomeViewModel.cs
+++ ClimbWeb/Climb/ViewModels/Users/HomeViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Climb.Data;
 using Climb.Models;
 using Climb.Services;
+using Microsoft.EntityFrameworkCore;
 
 namespace Climb.ViewModels.Users
 {
@@ -13,24 +15,46 @@ namespace Climb.ViewModels.Users
         public bool IsViewingUserHome => User.Id == HomeUser.Id;
         public IReadOnlyList<Set> RecentSets { get; }
         public IReadOnlyList<Set> AvailableSets { get; }
+        public IReadOnlyList<LeagueUser> SharedLeagues { get; }
+        public IReadOnlyList<SetRequest> SetRequests { get; }
+        public bool ShowSetRequests { get; }
 
-        private HomeViewModel(ApplicationUser user, ApplicationUser homeUser, string profilePic, IReadOnlyList<Set> recentSets, IReadOnlyList<Set> availableSets)
+        private HomeViewModel(ApplicationUser user, ApplicationUser homeUser, string profilePic, IReadOnlyList<Set> recentSets, IReadOnlyList<Set> availableSets, IReadOnlyList<SetRequest> setRequests, bool showSetRequests)
             : base(user)
         {
             HomeUser = homeUser;
             ProfilePic = profilePic;
             RecentSets = recentSets;
             AvailableSets = availableSets;
+            SetRequests = setRequests;
+            ShowSetRequests = showSetRequests;
+
+            SharedLeagues = homeUser.LeagueUsers.Where(lu => user.LeagueUsers.Any(lu2 => lu2.LeagueID == lu.LeagueID)).ToArray();
         }
 
-        public static HomeViewModel Create(ApplicationUser user, ApplicationUser homeUser, ICdnService cdnService)
+        public static async Task<HomeViewModel> CreateAsync(ApplicationUser user, ApplicationUser homeUser, ICdnService cdnService, ApplicationDbContext dbContext)
         {
             var profilePic = homeUser.GetProfilePicUrl(cdnService);
             var sets = homeUser.LeagueUsers.SelectMany(lu => lu.P1Sets.Union(lu.P2Sets)).ToArray();
             var recentSets = sets.Where(s => s.IsComplete).Take(10).ToArray();
             var availableSets = sets.Where(s => !s.IsComplete).Take(10).ToArray();
 
-            return new HomeViewModel(user, homeUser, profilePic, recentSets, availableSets);
+#if DEBUG
+            const bool showSetRequests = true;
+#else
+            var showSetRequests = user.Id == homeUser.Id;
+#endif 
+
+            IReadOnlyList<SetRequest> setRequests = null;
+            if(showSetRequests)
+            {
+                setRequests = await dbContext.SetRequests
+                    .Where(sr => user.LeagueUsers.Any(lu => lu.ID == sr.ChallengedID))
+                    .OrderByDescending(sr => sr.DateCreated)
+                    .ToArrayAsync();
+            }
+
+            return new HomeViewModel(user, homeUser, profilePic, recentSets, availableSets, setRequests, showSetRequests);
         }
     }
 }
