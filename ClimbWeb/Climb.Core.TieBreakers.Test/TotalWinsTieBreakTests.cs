fabb76cb2fff89f831df4e9916c4ab0c535011d7
Unknown <steve.yeager@littlebytegames.com>
2018-07-20 10:53:41 -0700

    Add TotalWinsTieBreak

---
+++ ClimbWeb/Climb.Core.TieBreakers.Test/TotalWinsTieBreakTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Climb.Core.TieBreakers.Internal;
+using NUnit.Framework;
+
+namespace Climb.Core.TieBreakers.Test
+{
+    [TestFixture]
+    public class TotalWinsTieBreakTests
+    {
+        private TotalWinsTieBreak testObj;
+
+        [SetUp]
+        public void SetUp()
+        {
+            testObj = new TotalWinsTieBreak();
+        }
+
+        [Test]
+        public void GetUserScore_ReturnWinCounts()
+        {
+            var participants = new List<Participant> {AddParticipant(1, 3), AddParticipant(2, 5), AddParticipant(3, 4)};
+
+            var results = testObj.Evaluate(participants);
+
+            Assert.AreEqual(3, results[0].RoundPoints);
+            Assert.AreEqual(5, results[1].RoundPoints);
+            Assert.AreEqual(4, results[2].RoundPoints);
+        }
+
+        private static Participant AddParticipant(int id, int winCount)
+        {
+            var participant = new Participant(id, 0, 0);
+            for(var i = 0; i < winCount; i++)
+            {
+                participant.AddWin(0);
+            }
+
+            return participant;
+        }
+    }
+}
--- ClimbWeb/Climb.Core.TieBreakers/API/TieBreakers/TotalWinsTieBreak.cs
+++ ClimbWeb/Climb.Core.TieBreakers/API/TieBreakers/TotalWinsTieBreak.cs
@@ -1,7 +1,12 @@
-﻿namespace Climb.Core.TieBreakers.Internal
+﻿using System.Collections.Generic;
+
+namespace Climb.Core.TieBreakers.Internal
 {
-    internal class TotalWinsTieBreak
+    internal class TotalWinsTieBreak : TieBreakAttempt
     {
-        
+        protected override int GetUserScore(IReadOnlyList<Participant> participants, Participant current)
+        {
+            return current.Wins;
+        }
     }
 }
