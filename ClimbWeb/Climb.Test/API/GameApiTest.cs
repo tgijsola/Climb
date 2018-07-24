35579b7eb44611bb9b593847db68be5536bb8305
Unknown <steve.yeager@littlebytegames.com>
2018-07-24 07:15:27 -0700

    Remove GameApi AddCharacter and AddStage

--- ClimbWeb/Climb.Test/API/GameApiTest.cs
+++ ClimbWeb/Climb.Test/API/GameApiTest.cs
@@ -4,9 +4,6 @@ using Climb.API;
 using Climb.Data;
 using Climb.Extensions;
 using Climb.Models;
-using Climb.Requests.Games;
-using Climb.Services.ModelServices;
-using Climb.Test.Fakes;
 using Climb.Test.Utilities;
 using Microsoft.Extensions.Logging;
 using NSubstitute;
@@ -18,18 +15,15 @@ namespace Climb.Test.Api
     public class GameApiTest
     {
         private GameApi testObj;
-        private IGameService gameService;
         private ApplicationDbContext dbContext;
 
         [SetUp]
         public void SetUp()
         {
-            gameService = Substitute.For<IGameService>();
             dbContext = DbContextUtility.CreateMockDb();
-            var userManager = new FakeUserManager();
             var logger = Substitute.For<ILogger<GameApi>>();
 
-            testObj = new GameApi(logger, dbContext, gameService);
+            testObj = new GameApi(logger, dbContext);
         }
 
         [Test]
@@ -50,46 +44,6 @@ namespace Climb.Test.Api
             var result = await testObj.Get(0);
 
             ControllerUtility.AssertStatusCode(result, HttpStatusCode.NotFound);
-        }
-
-        [Test]
-        public async Task Create_Valid_CreatedResult()
-        {
-            var request = new CreateRequest();
-
-            gameService.Create(request).Returns(new Game());
-
-            var result = await testObj.Create(request);
-
-            ControllerUtility.AssertStatusCode(result, HttpStatusCode.Created);
-        }
-
-        [Test]
-        public async Task AddCharacter_Valid_Created()
-        {
-            var game = GameUtility.Create(dbContext, 0, 0);
-            var request = new AddCharacterRequest(game.ID, "Char1");
-            gameService.AddCharacter(request).Returns(new Character());
-
-            var result = await testObj.AddCharacter(request);
-            var resultObj = result.GetObject<Character>();
-
-            ControllerUtility.AssertStatusCode(result, HttpStatusCode.Created);
-            Assert.IsNotNull(resultObj);
-        }
-
-        [Test]
-        public async Task AddStage_Valid_Created()
-        {
-            var game = GameUtility.Create(dbContext, 0, 0);
-            var request = new AddStageRequest(game.ID, "Stage1");
-            gameService.AddStage(request).Returns(new Stage());
-
-            var result = await testObj.AddStage(request);
-            var resultObj = result.GetObject<Stage>();
-
-            ControllerUtility.AssertStatusCode(result, HttpStatusCode.Created);
-            Assert.IsNotNull(resultObj);
         }
     }
 }
--- ClimbWeb/Climb/API/GameApi.cs
+++ ClimbWeb/Climb/API/GameApi.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Net;
 using System.Threading.Tasks;
 using Climb.Attributes;
 using Climb.Data;
 using Climb.Models;
-using Climb.Requests.Games;
-using Climb.Services.ModelServices;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
@@ -16,13 +13,11 @@ namespace Climb.API
     public class GameApi : BaseApi<GameApi>
     {
         private readonly ApplicationDbContext dbContext;
-        private readonly IGameService gameService;
 
-        public GameApi(ILogger<GameApi> logger, ApplicationDbContext dbContext, IGameService gameService)
+        public GameApi(ILogger<GameApi> logger, ApplicationDbContext dbContext)
             : base(logger)
         {
             this.dbContext = dbContext;
-            this.gameService = gameService;
         }
 
         [HttpGet("/api/v1/games/{gameID:int}")]
@@ -40,56 +35,6 @@ namespace Climb.API
             }
 
             return CodeResult(HttpStatusCode.OK, game);
-        }
-
-        [HttpPost("/api/v1/games/create")]
-        [SwaggerResponse(HttpStatusCode.Created, typeof(Game))]
-        [SwaggerResponse(HttpStatusCode.Conflict, typeof(string), "Game name is taken.")]
-        public async Task<IActionResult> Create(CreateRequest request)
-        {
-            try
-            {
-                var game = await gameService.Create(request);
-                return CodeResultAndLog(HttpStatusCode.Created, game, "Game created.");
-            }
-            catch(Exception exception)
-            {
-                return GetExceptionResult(exception, request);
-            }
-        }
-
-        [HttpPost("/api/v1/addCharacter")]
-        [SwaggerResponse(HttpStatusCode.Created, typeof(Character))]
-        [SwaggerResponse(HttpStatusCode.NotFound, typeof(string), "Could not find game.")]
-        [SwaggerResponse(HttpStatusCode.Conflict, typeof(string), "Character name is taken.")]
-        public async Task<IActionResult> AddCharacter(AddCharacterRequest request)
-        {
-            try
-            {
-                var character = await gameService.AddCharacter(request);
-                return CodeResultAndLog(HttpStatusCode.Created, character, $"New character {character.Name} created.");
-            }
-            catch(Exception exception)
-            {
-                return GetExceptionResult(exception, request);
-            }
-        }
-
-        [HttpPost("/api/v1/addStage")]
-        [SwaggerResponse(HttpStatusCode.Created, typeof(Stage))]
-        [SwaggerResponse(HttpStatusCode.NotFound, typeof(string), "Could not find game.")]
-        [SwaggerResponse(HttpStatusCode.Conflict, typeof(string), "Stage name is taken.")]
-        public async Task<IActionResult> AddStage(AddStageRequest request)
-        {
-            try
-            {
-                var stage = await gameService.AddStage(request);
-                return CodeResultAndLog(HttpStatusCode.Created, stage, $"New stage {stage.Name} created.");
-            }
-            catch(Exception exception)
-            {
-                return GetExceptionResult(exception, request);
-            }
         }
 
         [HttpGet("/api/v1/games")]
