06cff144ba8acc46fa6851bc8a45abf6b4362909
Unknown <steve.yeager@littlebytegames.com>
2018-07-24 20:50:24 -0700

    Code cleanup

--- ClimbWeb/Climb/Startup.cs
+++ ClimbWeb/Climb/Startup.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Reflection;
+﻿using System.Reflection;
 using Climb.Data;
 using Climb.Services;
 using Climb.Services.ModelServices;
@@ -8,7 +7,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
