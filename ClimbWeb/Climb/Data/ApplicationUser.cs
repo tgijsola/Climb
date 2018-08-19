ae92be7ea156efd7272ad6bb1f848f894f44a3bf
Unknown <steve.yeager@littlebytegames.com>
2018-08-19 11:01:49 -0700

    Move getting user's profile pic url to cdn service

--- ClimbWeb/Climb/Data/ApplicationUser.cs
+++ ClimbWeb/Climb/Data/ApplicationUser.cs
@@ -1,7 +1,5 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using Climb.Models;
-using Climb.Services;
 using Microsoft.AspNetCore.Identity;
 
 namespace Climb.Data
@@ -13,19 +11,5 @@ namespace Climb.Data
 
         public List<OrganizationUser> Organizations { get; set; }
         public List<LeagueUser> LeagueUsers { get; set; }
-
-        public string GetProfilePicUrl(ICdnService cdnService)
-        {
-            const int defaultPicturesCount = 24;
-
-            if(string.IsNullOrWhiteSpace(ProfilePicKey))
-            {
-                var idHash = Id.Select(c => (int)c).Sum();
-                var defaultID = idHash % defaultPicturesCount;
-                return $"/images/profile-default/profile-default-{defaultID}.jpg";
-            }
-
-            return cdnService.GetImageUrl(ProfilePicKey, ClimbImageRules.ProfilePic);
-        }
     }
 }
