0e3c27cc02d82578e723d762759d25cdce12ca0a
Unknown <steve.yeager@littlebytegames.com>
2018-08-19 18:56:05 -0700

    Designate newcomers on leagues/home

--- ClimbWeb/Climb/ViewModels/Leagues/HomeViewModel.cs
+++ ClimbWeb/Climb/ViewModels/Leagues/HomeViewModel.cs
@@ -10,6 +10,7 @@ namespace Climb.ViewModels.Leagues
         public League League { get; }
         public bool IsMember { get; }
         public IReadOnlyList<LeagueUser> Members { get; }
+        public IReadOnlyList<LeagueUser> Newcomers { get; }
         public bool CanStartSeason { get;  }
 
         public HomeViewModel(ApplicationUser user, League league)
@@ -18,7 +19,8 @@ namespace Climb.ViewModels.Leagues
             League = league;
 
             league.Members.Sort();
-            Members = league.Members;
+            Members = league.Members.Where(lu => !league.IsMemberNew(lu)).ToList();
+            Newcomers = league.Members.Where(league.IsMemberNew).ToList();
             IsMember = league.Members.Any(lu => lu.UserID == user?.Id);
 
 #if DEBUG
